0b6e0c6a6e08d44bea220ce4137139c2a8ebe0a8
seanacres <6416690+seanacres@users.noreply.github.com>
2019-07-17 15:49:42 -0400

    Set up project and necessary views

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Sean Acres on 7/17/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
