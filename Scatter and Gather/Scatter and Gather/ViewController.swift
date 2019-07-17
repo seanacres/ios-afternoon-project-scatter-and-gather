7fba03769834d867e8a8df2d89bdde27fb84ec3b
seanacres <6416690+seanacres@users.noreply.github.com>
2019-07-17 15:50:16 -0400

    Set up views programatically

--- Scatter and Gather/Scatter and Gather/ViewController.swift
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isScattered: Bool?
+    var labelArray: [UILabel] = []
+    var imageView: UIImageView = UIImageView(image: UIImage(named: "lambda_logo"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureViews()
     }
 
-
+    @IBAction func toggleButtonPressed(_ sender: Any) {
+    }
+    
+    func configureViews() {
+        
+        // string to create labels for
+        let lambdaString = "Lambda"
+        
+        // set up stackview
+        let stackView = UIStackView()
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
+            ])
+        
+        // create labels for each character in string and add to stackview
+        for c in lambdaString {
+            let label = UILabel()
+            label.text = "\(c)"
+            label.font = UIFont.boldSystemFont(ofSize: 50.0)
+            label.textAlignment = .center
+            labelArray.append(label)
+            stackView.addArrangedSubview(label)
+        }
+        
+        // set up imageview
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 58)
+            ])
+    }
+    
+    
 }
 
